36f96d7225e2e3d9cd9a2916d35b10d37a221eec
Pankaj Bepari <beparipankaj@gmail.com>
2021-11-05 01:24:16 +0600

    Class-4: Even Handeling

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
 
 export default class Clock extends Component {
-  state = { date: new Date() };
+  state = {
+    date: new Date(),
+    local: "en-US",
+  };
 
-  tick() {
+  tick () {
     this.setState({ date: new Date() });
   }
-  componentDidMount() {
+  componentDidMount(){
     this.clockTimer = setInterval(() => this.tick(), 1000);
   }
 
-  componentWillUnmount() {
+  componentWillUnmount()  {
     clearInterval(this.clockTimer);
-  }
+  };
+
+  clickHandle = () => {
+    this.setState({ local: this.state.local === "en-US" ? "bn-BD" : "en-US" });
+
+  };
 
   render() {
+    const { date, local } = this.state;
     return (
       <div>
         <h1>
-          The Courrent Time is{" "}
-          <span>{this.state.date.toLocaleTimeString()}</span>
+          The Courrent Time is <span>{date.toLocaleTimeString(local)}</span>
         </h1>
+        <button onClick={this.clickHandle}>Click Me!</button>
       </div>
     );
   }
