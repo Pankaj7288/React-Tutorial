0128d7e6770b11f1a71173d0b070819e8f1ba673
Pankaj Bepari <beparipankaj@gmail.com>
2021-11-04 15:44:25 +0600

    Class-2: React Component & props

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
+import Rcc from "./Rcc";
+import Rfc from "./Rfc";
 
+export default function App() {
+  return (
+    <div>
+      <Rfc myName="Pankaj" />
+      <Rcc spouseName="Satu Samonty" />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 const render = document.getElementById("root");
 const element = (
   <>
-    <h1>Hello, World!</h1>
+    <App />
   </>
 );
 
