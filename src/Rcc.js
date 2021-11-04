e1e2850f6534df38cc226b24a364a3b3dba4d456
Pankaj Bepari <beparipankaj@gmail.com>
2021-11-05 01:35:02 +0600

    props style change

--- src/Rcc.js
+++ src/Rcc.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 export default class Rcc extends Component {
   render() {
+    const {spouseName} = this.props;
     return (
       <div>
-        <h2>Hello {this.props.spouseName}</h2>
+        <h2>Hello {spouseName}</h2>
       </div>
     );
   }
